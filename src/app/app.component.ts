2beea8f910ac97e08a31ff78dd203f54ecab5af8
Saul Mijael Choquehuanca Huanca <saulchoque123@gmail.com>
2025-03-11 01:24:58 -0400

    audio and some images fixed
    lol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, ViewChild, ElementRef, AfterViewInit, HostListener } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { routeTransition } from './animations/route-transition';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,10 +10,27 @@ import { routeTransition } from './animations/route-transition';
   styleUrls: ['./app.component.scss'],
   animations: [routeTransition]
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'noeliacumple';
 
   router = inject(Router);
+
+  @ViewChild('backgroundAudio') backgroundAudio!: ElementRef<HTMLAudioElement>;
+
+  ngOnInit() {
+    // Código de inicialización si es necesario
+  }
+
+  ngAfterViewInit() {
+    // Espera a que el usuario interactúe con la página para reproducir el audio
+    document.addEventListener('click', this.playAudio.bind(this), { once: true });
+  }
+
+  playAudio() {
+    this.backgroundAudio.nativeElement.play().catch(error => {
+      console.error('Error al reproducir el audio:', error);
+    });
+  }
 
   /*
   @HostListener('window:touchstart', ['$event'])
@@ -20,5 +38,5 @@ export class AppComponent {
     console.log('Pantalla tocada');
     this.router.navigate(['/b']); // Cambia '/nueva-ruta' por la ruta a la que deseas navegar
   }
-*/
+  */
 }
